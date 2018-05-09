f7c5dc5a66923a0abda8986765cb39b20bc73cfe
fabianramos01 <fabian.ramos01@uptc.edu.co>
2018-05-08 22:33:08 -0500

    Descarga el archivo .xml según la busqueda

--- src/controller/ConstantList.java
+++ src/controller/ConstantList.java
@@ -2,7 +2,7 @@ package controller;
 
 public class ConstantList {
 
-	public static final String WEB_INIT_PATH = "https://photopin.com/free-photos/";
-	public static final String WEB_END_PATH = "&search_image=.xml";
+	public static final String WEB_INIT_PATH = "https://alpha.wallhaven.cc/search?q=";
+	public static final String WEB_END_PATH = "&search_image=";
 	public static final String FILE_PATH = "images.xml";
 }
--- src/persistence/FileManager.java
+++ src/persistence/FileManager.java
@@ -1,6 +1,5 @@
 package persistence;
 
-import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,11 +22,12 @@ import controller.ConstantList;
 public class FileManager {
 
 	public static void loadImages(String image) throws IOException {
-		// Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("172.16.0.73",
-		// 8080));
-		URL website = new URL(ConstantList.WEB_INIT_PATH + image);
-		try (InputStream in = website.openStream()) {
-		    Files.copy(in, Paths.get(ConstantList.FILE_PATH), StandardCopyOption.REPLACE_EXISTING);
+		URLConnection website = new URL(ConstantList.WEB_INIT_PATH + image + ConstantList.WEB_END_PATH)
+				.openConnection();
+		website.addRequestProperty("User-Agent",
+				"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+		try (InputStream in = website.getInputStream()) {
+			Files.copy(in, Paths.get(ConstantList.FILE_PATH), StandardCopyOption.REPLACE_EXISTING);
 		}
 	}
 
